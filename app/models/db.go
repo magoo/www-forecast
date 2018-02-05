a5d62cfcbd479c0da25d39dddf8d82dcc56dad2a
Ryan McGeehan <billygoatmagoo@gmail.com>
2018-02-05 12:57:41 -0800

    forecast tool

---
+++ app/models/db.go
@@ -0,0 +1,24 @@
+package models
+
+import (
+		"github.com/aws/aws-sdk-go/aws"
+	  "github.com/aws/aws-sdk-go/aws/session"
+		"github.com/aws/aws-sdk-go/aws/credentials"
+	  "github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+var sess, _ = session.NewSession(&aws.Config{
+		Region: aws.String("us-west-2"),
+		Credentials: credentials.NewSharedCredentials("", "magoo"),
+	},
+)
+
+var Svc = dynamodb.New(sess)
+
+func DbConnect(){
+
+
+
+
+
+}
