9daf35a9a2988d3f76bbbaf30a3734d5094bcc53
Ryan McGeehan <billygoatmagoo@gmail.com>
2018-05-29 16:40:30 -0700

    Fixed "record" bug.
    Still having issues with the record feature. Had to force the
    `dynamodbav` type to a stringset to allow appending of strings. Highly
    confusing.
    
    Had to degrade the terraform script to get dev DB working with old
    application.

--- tf/dynamodb.tf
+++ tf/dynamodb.tf
@@ -3,14 +3,14 @@ provider "aws" {
   region              = "us-west-1"
 }
 
-resource "aws_dynamodb_table" "questions" {
-  name           = "questions-tf"
+resource "aws_dynamodb_table" "scenarios" {
+  name           = "scenarios-tf"
   read_capacity  = 1
   write_capacity = 1
-  hash_key       = "id"
+  hash_key       = "sid"
 
   attribute {
-     name = "id"
+     name = "sid"
      type = "S"
   }
 
@@ -28,29 +28,127 @@ resource "aws_dynamodb_table" "questions" {
   }
 }
 
-resource "aws_dynamodb_table" "answers" {
-  name           = "answers-tf"
+resource "aws_dynamodb_table" "forecasts" {
+  name           = "forecasts-tf"
   read_capacity  = 1
   write_capacity = 1
-  hash_key       = "id"
+  hash_key       = "sid"
   range_key       = "ownerid"
 
   attribute {
-     name = "id"
+     name = "sid"
      type = "S"
   }
   attribute {
      name = "ownerid"
      type = "S"
   }
+
+  global_secondary_index {
+    name               = "sid-index"
+    hash_key           = "sid"
+    write_capacity     = 1
+    read_capacity      = 1
+    projection_type    = "ALL"
+  }
+
+}
+
+resource "aws_dynamodb_table" "estimates" {
+  name           = "estimates-tf"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "eid"
+
   attribute {
-     name = "qid"
+     name = "eid"
+     type = "S"
+  }
+
+  attribute {
+     name = "ownerid"
      type = "S"
   }
 
   global_secondary_index {
-    name               = "qid-index"
-    hash_key           = "qid"
+    name               = "ownerid-index"
+    hash_key           = "ownerid"
+    write_capacity     = 1
+    read_capacity      = 1
+    projection_type    = "ALL"
+  }
+}
+
+resource "aws_dynamodb_table" "ranges" {
+  name           = "ranges-tf"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "eid"
+  range_key       = "ownerid"
+
+  attribute {
+     name = "eid"
+     type = "S"
+  }
+  attribute {
+     name = "ownerid"
+     type = "S"
+  }
+
+  global_secondary_index {
+    name               = "eid-index"
+    hash_key           = "eid"
+    write_capacity     = 1
+    read_capacity      = 1
+    projection_type    = "ALL"
+  }
+
+}
+
+resource "aws_dynamodb_table" "ranks" {
+  name           = "ranks-tf"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "rid"
+
+  attribute {
+     name = "rid"
+     type = "S"
+  }
+
+  attribute {
+     name = "ownerid"
+     type = "S"
+  }
+
+  global_secondary_index {
+    name               = "ownerid-index"
+    hash_key           = "ownerid"
+    write_capacity     = 1
+    read_capacity      = 1
+    projection_type    = "ALL"
+  }
+}
+
+resource "aws_dynamodb_table" "sorts" {
+  name           = "sorts-tf"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "rid"
+  range_key       = "ownerid"
+
+  attribute {
+     name = "rid"
+     type = "S"
+  }
+  attribute {
+     name = "ownerid"
+     type = "S"
+  }
+
+  global_secondary_index {
+    name               = "rid-index"
+    hash_key           = "rid"
     write_capacity     = 1
     read_capacity      = 1
     projection_type    = "ALL"
