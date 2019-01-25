406d91ce48805d4e50c8d15ea26c1633252b7867
Ryan McGeehan <magoo@users.noreply.github.com>
2019-01-24 16:05:48 -0800

    Brier Score Patch + Tests

--- tests/reveltests.go
+++ tests/reveltests.go
@@ -4,20 +4,20 @@ import (
 	"github.com/revel/revel/testing"
 )
 
-type AppTest struct {
+type RevelTest struct {
 	testing.TestSuite
 }
 
-func (t *AppTest) Before() {
+func (t *RevelTest) Before() {
 	println("Set up")
 }
 
-func (t *AppTest) TestThatIndexPageWorks() {
+func (t *RevelTest) TestThatIndexPageWorks() {
 	t.Get("/")
 	t.AssertOk()
 	t.AssertContentType("text/html; charset=utf-8")
 }
 
-func (t *AppTest) After() {
+func (t *RevelTest) After() {
 	println("Tear down")
 }
